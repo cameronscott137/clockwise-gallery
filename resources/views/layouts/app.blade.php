ce1a695d1927e3d990606553ee9d068b8560a039
cameronscott137 <cscott@inquiri.com>
2019-10-16 09:23:02 -0400

    wip

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,14 @@
     <!-- Styles -->
     <link rel="stylesheet" href="https://use.typekit.net/vto7vtu.css">
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16x16.png') }}">
+    <link rel="manifest" href="{{ asset('site.webmanifest') }}">
+    <link rel="mask-icon" href="{{ asset('safari-pinned-tab.svg') }}" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#1f1f1f">
+    <meta name="theme-color" content="#ffffff">
 </head>
 
 <body class="bg-gray-100 h-screen antialiased leading-none">
