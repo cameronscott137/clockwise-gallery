f1f763957fa3b0b562871eb450bd6debbe571005
cameronscott137 <cscott@inquiri.com>
2019-12-03 20:04:08 -0500

    Removes extra quotation from title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name') }} | @yield('title')"</title>
+    <title>{{ config('app.name') }} | @yield('title')</title>
 
     <!-- Styles -->
     <link rel="stylesheet" href="https://use.typekit.net/vto7vtu.css">
